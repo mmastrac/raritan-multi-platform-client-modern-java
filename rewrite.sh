e20af5d2e8f71f2fd5753ef9c6180d7895019fcb
Matt Mastracci <matthew@mastracci.com>
2024-06-22 10:53:53 -0600

    Make rewriter way more robust

--- rewrite.sh
+++ rewrite.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
 set -eu -o pipefail
 
-JAVA=/usr/local/opt/openjdk\@11/bin/java
+JAVA=$(brew --prefix openjdk\@11 || brew --prefix openjdk)/bin/java
 BUILD=build/
 DIST=build/dist/
+INPUT=inputs/orig/mpc-installer.MPC_7.0.3.5.62.jar.zip
+INPUT_MD5=82e169d048149182fbfc6a569fe147e1
+DOWNLOAD_URL="https://d3b2us605ptvk2.cloudfront.net/download/kxii/v2.7.0/mpc-installer.MPC_7.0.3.5.62.jar.zip"
+TEMP_INPUT=/tmp/mpc.jar
+
+echo "Testing Java ($JAVA)..."
+$JAVA --version || echo "Brew Java needs to be installed"
 
 rm -rf $BUILD || true
 mkdir $BUILD
 
+calculate_md5() {
+    if command -v md5sum &> /dev/null; then
+        md5sum "$1" | awk '{ print $1 }'
+    else
+        md5 -q "$1"
+    fi
+}
+
+# Check if the input file exists and has the correct MD5 hash
+if [[ -f "$INPUT" ]]; then
+    EXISTING_MD5=$(calculate_md5 "$INPUT")
+    if [[ "$EXISTING_MD5" == "$INPUT_MD5" ]]; then
+        echo "File already exists and MD5 matches. No download needed."
+    else
+        echo "File exists but MD5 does not match. Downloading again."
+        curl -o "$TEMP_INPUT" "$DOWNLOAD_URL"
+        DOWNLOADED_MD5=$(calculate_md5 "$TEMP_INPUT")
+        if [[ "$DOWNLOADED_MD5" == "$INPUT_MD5" ]]; then
+            mv "$TEMP_INPUT" "$INPUT"
+        else
+            echo "Downloaded file MD5 does not match. Exiting."
+            exit 1
+        fi
+    fi
+else
+    echo "File does not exist. Downloading."
+    curl -o "$TEMP_INPUT" "$DOWNLOAD_URL"
+    DOWNLOADED_MD5=$(calculate_md5 "$TEMP_INPUT")
+    if [[ "$DOWNLOADED_MD5" == "$INPUT_MD5" ]]; then
+        mv "$TEMP_INPUT" "$INPUT"
+    else
+        echo "Downloaded file MD5 does not match. Exiting."
+        exit 1
+    fi
+fi
+
 echo Extracting installer...
-unzip inputs/orig/mpc-installer.MPC_7.0.3.5.62.jar.zip -d $BUILD > build/install.log
+unzip $INPUT -d $BUILD > build/install.log
 $JAVA -jar $BUILD/mpc-installer.MPC_7.0.3.5.62.jar -options inputs/orig/install.properties 2>&1 > build/install.log
 
 rm -rf $BUILD
